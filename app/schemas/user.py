a672e01ccd9f2be1eed7f00d998d5318dcca15a6
Abdul Aziz <abdulaziz@example.com>
2026-09-21 23:26:16 +0500

    chore(tests): resolve pydantic and passlib warnings

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, ConfigDict
 from uuid import UUID
 from datetime import datetime
 from typing import Optional
@@ -16,8 +16,7 @@ class UserRead(UserBase):
     created_at: datetime
     updated_at: datetime
 
-    class Config:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
 
 class UserUpdate(BaseModel):
     email: Optional[EmailStr] = None
